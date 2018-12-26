f0419a566120c59e94dde9655c0bd2118b443280
shinyay <shinya.com@gmail.com>
2018-12-26 14:25:13 +0900

    Initial Commit
    Add code

---
+++ src/main/java/io/pivotal/syanagihara/functions/random/RandomApplication.java
@@ -0,0 +1,23 @@
+package io.pivotal.syanagihara.functions.random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class RandomApplication {
+
+    @Bean
+    public Supplier<Integer> random() {
+        return () -> new Random().nextInt(100) + 1;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomApplication.class, args);
+    }
+
+}
+
