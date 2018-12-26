41bd610681fc1c2ee6671163c41e4629556ea2d7
shinyay <shinya.com@gmail.com>
2018-12-26 15:06:51 +0900

    Modify from Supplier to Function

--- src/main/java/io/pivotal/syanagihara/functions/RandomApplication.java
+++ src/main/java/io/pivotal/syanagihara/functions/RandomApplication.java
@@ -6,14 +6,13 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Random;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 @SpringBootApplication
 public class RandomApplication {
 
-    @Bean
-    public Supplier<Integer> random() {
-        return () -> new Random().nextInt(100) + 1;
+    @Bean(name = "number")
+    public Function<Integer, Integer> random() {
+        return s -> new Random().nextInt(s) + 1;
     }
 
     public static void main(String[] args) {
